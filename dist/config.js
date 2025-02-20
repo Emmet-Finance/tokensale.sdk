ab93fd207f279a02e7f443567d8938b7c9c016ff
Mithreum <120666231+Mithreum@users.noreply.github.com>
2025-02-20 14:47:37 +0000

    Build: (823250b) add: RPC nodes

--- dist/config.js
+++ dist/config.js
@@ -10,6 +10,13 @@ exports.mainnetConfig = {
         "https://binance.llamarpc.com",
         "https://bsc-rpc.publicnode.com",
         "https://bsc.blockrazor.xyz",
+        "https://rpc-bsc.48.club",
+        "https://bsc-dataseed1.ninicoin.io",
+        "https://bsc-dataseed2.ninicoin.io",
+        "https://bsc-dataseed3.bnbchain.org",
+        "https://bsc-pokt.nodies.app",
+        "https://1rpc.io/bnb",
+        "https://bsc-rpc.publicnode.com",
     ],
     usdtAddress: "0x55d398326f99059ff775485246999027b3197955"
 };
@@ -21,16 +28,7 @@ exports.testnetConfig = {
     rpcs: [
         "https://bsc-testnet-rpc.publicnode.com",
         "https://endpoints.omniatech.io/v1/bsc/testnet/public",
-        "https://bsc-dataseed.bnbchain.org",
-        "https://rpc.ankr.com/bsc",
-        "https://rpc-bsc.48.club",
-        "https://bsc-dataseed1.ninicoin.io",
-        "https://bsc-dataseed2.ninicoin.io",
-        "https://bsc-dataseed3.bnbchain.org",
-        "https://bsc-pokt.nodies.app",
-        "https://1rpc.io/bnb",
-        "https://bsc-rpc.publicnode.com",
     ],
     usdtAddress: "0xe8ab99474b8bb70d0cf258623fcac8d8681a1e50"
 };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFFYSxRQUFBLGFBQWEsR0FBWTtJQUNsQyxPQUFPLEVBQUUsR0FBRztJQUNaLFlBQVksRUFBRSw0Q0FBNEM7SUFDMUQsY0FBYyxFQUFFLDRDQUE0QztJQUM1RCxnQkFBZ0IsRUFBRSw0Q0FBNEM7SUFDOUQsSUFBSSxFQUFFO1FBQ0YsOEJBQThCO1FBQzlCLGdDQUFnQztRQUNoQyw0QkFBNEI7S0FDL0I7SUFDRCxXQUFXLEVBQUUsNENBQTRDO0NBQzVELENBQUE7QUFFWSxRQUFBLGFBQWEsR0FBWTtJQUNsQyxPQUFPLEVBQUUsR0FBRztJQUNaLFlBQVksRUFBRSw0Q0FBNEM7SUFDMUQsY0FBYyxFQUFFLDRDQUE0QztJQUM1RCxnQkFBZ0IsRUFBRSw0Q0FBNEM7SUFDOUQsSUFBSSxFQUFFO1FBQ0Ysd0NBQXdDO1FBQ3hDLHNEQUFzRDtRQUN0RCxtQ0FBbUM7UUFDbkMsMEJBQTBCO1FBQzFCLHlCQUF5QjtRQUN6QixtQ0FBbUM7UUFDbkMsbUNBQW1DO1FBQ25DLG9DQUFvQztRQUNwQyw2QkFBNkI7UUFDN0IscUJBQXFCO1FBQ3JCLGdDQUFnQztLQUVuQztJQUNELFdBQVcsRUFBRSw0Q0FBNEM7Q0FDNUQsQ0FBQSJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFFYSxRQUFBLGFBQWEsR0FBWTtJQUNsQyxPQUFPLEVBQUUsR0FBRztJQUNaLFlBQVksRUFBRSw0Q0FBNEM7SUFDMUQsY0FBYyxFQUFFLDRDQUE0QztJQUM1RCxnQkFBZ0IsRUFBRSw0Q0FBNEM7SUFDOUQsSUFBSSxFQUFFO1FBQ0YsOEJBQThCO1FBQzlCLGdDQUFnQztRQUNoQyw0QkFBNEI7UUFDNUIseUJBQXlCO1FBQ3pCLG1DQUFtQztRQUNuQyxtQ0FBbUM7UUFDbkMsb0NBQW9DO1FBQ3BDLDZCQUE2QjtRQUM3QixxQkFBcUI7UUFDckIsZ0NBQWdDO0tBQ25DO0lBQ0QsV0FBVyxFQUFFLDRDQUE0QztDQUM1RCxDQUFBO0FBRVksUUFBQSxhQUFhLEdBQVk7SUFDbEMsT0FBTyxFQUFFLEdBQUc7SUFDWixZQUFZLEVBQUUsNENBQTRDO0lBQzFELGNBQWMsRUFBRSw0Q0FBNEM7SUFDNUQsZ0JBQWdCLEVBQUUsNENBQTRDO0lBQzlELElBQUksRUFBRTtRQUNGLHdDQUF3QztRQUN4QyxzREFBc0Q7S0FDekQ7SUFDRCxXQUFXLEVBQUUsNENBQTRDO0NBQzVELENBQUEifQ==
