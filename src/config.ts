8b649dcb71e7d472b2c080585cf01e84a9456af7
Mithras <dimabrook79@gmail.com>
2025-03-02 19:21:15 +0200

    add: token & staking addresses

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,16 @@ import { TConfig } from "./types";
 
 export const mainnetConfig: TConfig = {
     chainId: 56n,
-    emmetAddress: "0x6b30f76CecE9F92D27f0e9Ad78312E77709E74A5",
-    stakingAddress: "0x19Dc6ff2D315037adEa3a071C8aF9F2751B73b0A",
+    tokenAddresses: {
+        EMMET: "0x6b30f76CecE9F92D27f0e9Ad78312E77709E74A5",
+        NTM: "0xb8AA2a3bA6bEbE4ed7107028b26fae85a1044B57",
+        USDT: "0x55d398326f99059ff775485246999027b3197955"
+    },
+    stakingAddress: {
+        EMMET: "0x19Dc6ff2D315037adEa3a071C8aF9F2751B73b0A",
+        NTM: "0x8746A45910D0aF7e15C13f5Df10F072913387197",
+        USDT: ""
+    },
     tokensaleAddress: "0x02D4c32a5D68ABEE334d68f21C81012C37D625b7",
     rpcs: [
         "https://binance.llamarpc.com",
@@ -17,17 +25,23 @@ export const mainnetConfig: TConfig = {
         "https://1rpc.io/bnb",
         "https://bsc-rpc.publicnode.com",
     ],
-    usdtAddress: "0x55d398326f99059ff775485246999027b3197955"
 }
 
 export const testnetConfig: TConfig = {
     chainId: 97n,
-    emmetAddress: "0xd455eba1b88d175778968c45d161bac6fd5c8e60",
-    stakingAddress: "0xD914c365f5177AaC9E91D3477e43d0Ca10768d52",
+    tokenAddresses: {
+        EMMET: "0xd455eba1b88d175778968c45d161bac6fd5c8e60",
+        NTM: "",
+        USDT: "0xe8ab99474b8bb70d0cf258623fcac8d8681a1e50",
+    },
+    stakingAddress: {
+        EMMET: "0xD914c365f5177AaC9E91D3477e43d0Ca10768d52",
+        NTM: "",
+        USDT: ""
+    }, 
     tokensaleAddress: "0x4558C2a410538a5f3dF05fE75619c847c0aAed34",
     rpcs: [
         "https://bsc-testnet-rpc.publicnode.com",
         "https://endpoints.omniatech.io/v1/bsc/testnet/public",
     ],
-    usdtAddress: "0xe8ab99474b8bb70d0cf258623fcac8d8681a1e50"
 }
